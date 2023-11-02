ca825cd1cd045784da87b0feb75ceed5b9aa1dbb
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-11-02 20:54:56 +0000

    feat: calling the promise using then() and catch()

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,21 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
+const handleResolve = (response) => {
+  console.log(`Yay! Promise resolved with response: ${response}`);
+};
 
-// Your solution(s) to exercise001 here!
+const handleReject = (error) => {
+  console.log(`Boo. Promise rejected with response: ${error}`);
+};
+
+const value = promise.then(handleResolve).catch(handleReject);
